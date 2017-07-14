352d9486e971bacac3a7d2ae85e82b8da3469a9c
Baljinder udemy <baljinderbrar@hotmail.com>
2017-07-14 07:58:41 +0000

    added styleto flash, error

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -15,7 +15,7 @@ class ArticlesController < ApplicationController
     
     @article = Article.new(article_params)
     if @article.save
-      flash[:notice] = "article was successfully created"
+      flash[:success] = "article was successfully created"
       redirect_to article_path(@article)
     else
       render 'new'
@@ -33,7 +33,7 @@ class ArticlesController < ApplicationController
   def update
     @article = Article.find(params[:id])
     if @article.update(article_params)
-      flash[:notice] = "Article was successfully updated"
+      flash[:success] = "Article was successfully updated"
       redirect_to article_path(@article)
     else
       render 'edit'
@@ -44,7 +44,7 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
     @article.destroy
     
-    flash[:notice] = "Article was successfully destroyed"
+    flash[:danger] = "Article was successfully destroyed"
     redirect_to articles_path
   end
   
