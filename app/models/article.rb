92285c28dc4d89ec654e60e3b5a8e5de6094f26c
Baljinder udemy <baljinderbrar@hotmail.com>
2017-07-22 08:06:07 +0000

    create users table

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,4 @@
 class Article < ActiveRecord::Base
   validates :title, length: {minimum: 3, maximum: 50}
-  validates :description, length: {minimum: 3, maximum: 50}
+  validates :description, length: {minimum: 3, maximum: 5000}
 end
---
+++ app/models/user.rb
@@ -0,0 +1,4 @@
+class User < ActiveRecord::Base
+  #validates :title, length: {minimum: 3, maximum: 50}
+  #validates :description, length: {minimum: 3, maximum: 5000}
+end
