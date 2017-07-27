6115379d7b61e7e4b78c26b799911c381c02bb74
Baljinder udemy <baljinderbrar@hotmail.com>
2017-07-27 19:29:51 +0000

    update article, added user ref

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,7 @@
 class Article < ActiveRecord::Base
+  belongs_to :user
   validates :title, length: {minimum: 3, maximum: 50}
   validates :description, length: {minimum: 3, maximum: 5000}
+  validate :user_id, presence: true
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,11 @@
 class User < ActiveRecord::Base
-  #validates :title, length: {minimum: 3, maximum: 50}
-  #validates :description, length: {minimum: 3, maximum: 5000}
+  has_many :articles
+  
+  before_save { self.email = email.downcase }
+  
+  validates :username, presence: true, uniqueness: {case_sensitive: false}, length: {minimum: 2, maximum: 50}
+  VALIDATE_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :email, presence: true,  uniqueness: {case_sensitive: false}, 
+  format: {with: VALIDATE_EMAIL_REGEX},
+  length: {minimum: 3, maximum: 255}
 end
