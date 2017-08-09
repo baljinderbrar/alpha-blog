c537d0878898183a084284fc36d08b6fa983aef7
Baljinder udemy <baljinderbrar@hotmail.com>
2017-08-09 01:13:11 +0000

    fixed user related issues

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,5 +3,5 @@ class Article < ActiveRecord::Base
   
   validates :title, length: {minimum: 3, maximum: 50}
   validates :description, length: {minimum: 3, maximum: 5000}
-  validate :user_id, presence: true
+  validates :user_id, presence: true
 end
